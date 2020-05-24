c8b9f304503f2e023deda8a4b55dd80cf34e2864
Brendan Dagys <brendandagys@gmail.com>
2020-05-24 11:36:40 -0400

    Updated login page.

--- patient_alert/settings.py
+++ patient_alert/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '5q1_bl$_*q_wq-xwn_n7u1+#j&-yc8g($zlv5f+r(80_54xr#o'
+
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fake_key')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -31,10 +31,10 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'alerts.nbrhc@gmail.com'
-EMAIL_HOST_PASSWORD = 'nbrhcadmin1'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'fake_password')
 
-TWILIO_ACCOUNT_SID = 'AC426ca527987e550a5961ddd8f6b13fff'
-TWILIO_AUTH_TOKEN = '49b1aa1d35d6035a9941f1e36e5b7b01'
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_SID', 'fake_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', 'fake_auth_token')
 TWILIO_PHONE_NUMBER= '+17052303221'
 
 # SECURITY WARNING: don't run with debug turned on in production!
